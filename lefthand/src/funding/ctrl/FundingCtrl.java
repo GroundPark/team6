3964e0fe3eaad82e73cad73526bbc78c12c642f7
dlwkdgh96 <62927523+dlwkdgh96@users.noreply.github.com>
2021-11-24 19:04:08 +0900

    Update FundingCtrl.java

--- lefthand/src/funding/ctrl/FundingCtrl.java
+++ lefthand/src/funding/ctrl/FundingCtrl.java
@@ -2,62 +2,61 @@ package funding.ctrl;
 
 import java.io.*;
 import javax.servlet.*;
-import javax.servlet.annotation.WebServlet;
+import javax.servlet.annotation.*;
 import javax.servlet.http.*;
-import product.act.*;
-import product.vo.*;
 
-@WebServlet("*.funding")
+import act.Action;
+import act.PdtListAct;
+import act.PdtViewAct;
+import funding.act.*;
+import funding.vo.*;
+import vo.ActionForward;
+
+@WebServlet("*.fdg")
 public class FundingCtrl extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    
     public FundingCtrl() {
         super();
     }
-    
-    public void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    	request.setCharacterEncoding("utf-8");
-    	String requestUri = request.getRequestURI();
-    	String contextPath = request.getContextPath();
-    	String command = requestUri.substring(contextPath.length());
-    	
-    	ActionForward forward = null;
-    	Action action = null;
-    	
-    	switch (command) {
-    	case "/funding_list.pdt" :			
-    		action = new PdtListAct();
+
+	protected void doProcess(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		request.setCharacterEncoding("utf-8");
+		String requestUri = request.getRequestURI();
+		String contextPath = request.getContextPath();
+		String command = requestUri.substring(contextPath.length());
+
+		ActionForward forward = null;
+		Action action = null;
+
+		switch (command) {
+		case "/pdt_list.fdg" :	// 상품 목록 화면 요청
+			action = new FdgListAct();
 			break;
-		case "/funding_view.pdt" :		
+		case "/pdt_view.fdg" :	// 상품 상세 화면 요청
+			action = new FdgViewAct();
 			break;
 		}
-		
-    	
-    	try {
-    		forward = action.execute(request, response);
-    	}catch(Exception e) {
-    		e.printStackTrace();
-    	}
-    	
-    	if(forward != null) {
-    		if(forward.isRedirect()) {
-    			response.sendRedirect(forward.getPath());
-    		}else {
-    			RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
-    			dispatcher.forward(request, response);
-    		}
-    	}
+
+		try {
+			forward = action.execute(request, response);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+
+		if (forward != null) {
+			if (forward.isRedirect()) {
+				response.sendRedirect(forward.getPath());
+			} else {
+				RequestDispatcher dispatcher = request.getRequestDispatcher(forward.getPath());
+	    		dispatcher.forward(request, response);
+			}
+		}
 	}
-        
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doProcess(request, response);
 	}
-
-	
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doProcess(request, response);
 	}
-
 }
