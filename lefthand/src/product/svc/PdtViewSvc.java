cdcc4b094a5a86afa31f3e725c58d257886366c0
haaooo778 <94856495+haaooo778@users.noreply.github.com>
2021-11-28 22:13:08 +0900

    Add files via upload

--- lefthand/src/product/svc/PdtViewSvc.java
+++ lefthand/src/product/svc/PdtViewSvc.java
@@ -8,14 +8,24 @@ import product.vo.*;
 
 public class PdtViewSvc {
 	public ProductInfo getPdtInfo(String piid){
-		// 검색된 상품 목록을  ProductInfo형 인스턴스에 담아 리턴하는 메소드
-			ProductInfo pdtInfo = new ProductInfo();
-			Connection conn = getConnection();
-			ProductDao productDao = ProductDao.getInstance();
-			productDao.setConnection(conn);
-			pdtInfo = productDao.getPdtInfo(piid);
-			close(conn);
-				
-			return pdtInfo;
-		}
+		ProductInfo pdtInfo = new ProductInfo();
+		Connection conn = getConnection();
+		ProductDao productDao = ProductDao.getInstance();
+		productDao.setConnection(conn);
+		pdtInfo = productDao.getPdtInfo(piid);
+		close(conn);
+			
+		return pdtInfo;
+	}
+	
+	public ReviewInfo getRevInfo(String miid, String piid){
+		ReviewInfo revInfo = new ReviewInfo();
+		Connection conn = getConnection();
+		ProductDao productDao = ProductDao.getInstance();
+		productDao.setConnection(conn);
+		revInfo = productDao.getRevInfo(miid, piid);
+		close(conn);
+			
+		return revInfo;
+	}
 }
